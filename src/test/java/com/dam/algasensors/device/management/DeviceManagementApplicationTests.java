d930451fe1b25b4c647c4ad4430f3c9ea485ccf6
Douglas Moraes <douglas.am@gmail.com>
2025-05-30 23:42:58 -0300

    New project files

---
+++ src/test/java/com/dam/algasensors/device/management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.algasensors.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
