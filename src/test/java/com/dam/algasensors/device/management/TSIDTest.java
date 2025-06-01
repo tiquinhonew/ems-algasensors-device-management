9d69c8bd85d0f2fb9a82f87bbe167898aa71080f
Douglas Moraes <douglas.am@gmail.com>
2025-05-31 21:11:39 -0300

    9.5. Usando o TSID com Java

---
+++ src/test/java/com/dam/algasensors/device/management/TSIDTest.java
@@ -0,0 +1,22 @@
+package com.dam.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@DisplayName("TSID Test")
+class TSIDTest {
+
+    @Test
+    @DisplayName("Should Generate TSID")
+    void shouldGenerateTSID() {
+        TSID tsid = IdGenerator.generateTSID();
+        Assertions.assertThat(tsid.getInstant())
+                .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+    }
+
+}
